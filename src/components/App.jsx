2f3c348d0eb4837629478a7617fbffd55123ff86
YevheniiKravchukUA <Kravchukmail23@gmail.com>
2023-02-20 00:58:12 +0100

    finish do task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from 'components/Section/Section';
+import { FeedbackOptions } from 'components/Feedback/FeedbackOptions';
+import { Statistics } from 'components/Feedback/Statistics';
+import { Notification } from './Feedback/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const { good, neutral } = this.state;
+    return `${(((good + neutral) / this.countTotalFeedback()) * 100).toFixed(
+      0
+    )}%`;
+  }
+
+  onLeaveFeedback = evt => {
+    const stateValue = evt.target.name.toLowerCase();
+    this.setState(prevState => {
+      return {
+        [stateValue]: prevState[stateValue] + 1,
+      };
+    });
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
